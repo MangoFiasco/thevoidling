4a01dde636979f15565094a0e6b8c369cb23f954
Jiabo Hou <jiabo.hou@hotmail.com>
2019-03-17 19:51:22 -0400

    reorganize docker services, add void_swarm

--- thevoidling/__init__.py
+++ thevoidling/__init__.py
@@ -1,11 +1,20 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
+user = os.environ['DB_USER']
+password = os.environ['DB_PASS']
+host = os.environ['DB_HOST']
+port = os.environ['DB_PORT']
+db_name = os.environ['DB_NAME']
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://{user}:{password}@{host}:{port}/{db_name}"
 
 db = SQLAlchemy(app)
 
-from thevoidling import routes
+# from thevoidling import routes
 from thevoidling.models.roles import *
 
-db.create_all()
+# db.create_all()
+print(db.session.query(Roles)[0].id)
